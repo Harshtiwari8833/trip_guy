97b419a8a81f88dff0e4150ecaa64574f8ffd4f9
harshtiwari <harsh.tiwari@constems-ai.com>
2025-03-07 14:52:13 +0530

    feat: Implement route constants for navigation
    -   Created `Routes` object to define route constants for `LoginScreen`, `FillDetailsScreen`, and `TripEntryScreen`.
    -   Added new `Routes.kt` to hold these route constants for navigation.

---
+++ app/src/main/java/com/maverickbits/tripguy/routes/Routes.kt
@@ -0,0 +1,7 @@
+package com.maverickbits.tripguy.routes
+
+object Routes {
+    var LoginScreen = "google_login"
+    var FillDetailsScreen = "fill_details"
+    var TripEntryScreen = "trip_entry"
+}
