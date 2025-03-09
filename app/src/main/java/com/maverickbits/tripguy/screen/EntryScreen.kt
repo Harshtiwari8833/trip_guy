540b482b328de30986f72b2cc4e1b1508bd37740
harshtiwari <harsh.tiwari@constems-ai.com>
2025-03-09 23:26:07 +0530

    feat: Implement EntryScreen with UI components and Floating Action Button
    -   Created `EntryScreen` composable with `Scaffold` to structure the screen layout.
    -   Added a `Toolbar` as the top bar of the `Scaffold`.
    -   Implemented a `FloatingActionButton` with an "Add" icon to trigger bottom sheet.
    -   Added a `Box` container with `fillMaxSize` and `background` color.
    -   Implemented two columns inside the `Box` to display "Total" and "Individual" amounts.
    -   Updated total and individual amount view with white background and cardview.
    -   Updated text composable to show amount.
    -   Updated the `EntryScreen` with padding values from the `Scaffold`.
    -   Added colors to the `Text` components.
    -   Added `Preview` annotation to enable preview in Android Studio.
    -   Added dependency for material3 to build.gradle.kts.
    -   Updated `build.gradle.kts` to add required dependencies.
    -   Added padding to the `Box` container for spacing.
    -   Implemented `Column` composable to show total and individual amounts.
    -   Updated text composable to show "Total" and "Individual" title.
    -   Updated modifier to apply padding and background.
    -   Added icons to `FloatingActionButton` and updated content description.
    -   Added `Arrangement` and `Alignment` to the `Column` composables.
    -   Added `Row` and `Column` Composables to show a card view in the `EntryScreen`.
    -   Updated dependencies in `build.gradle.kts`.
    -   Removed unused imports.
    - Added `wrapContentHeight` to avoid UI overlapping.

--- app/src/main/java/com/maverickbits/tripguy/screen/EntryScreen.kt
+++ app/src/main/java/com/maverickbits/tripguy/screen/EntryScreen.kt
@@ -1,2 +1,135 @@
 package com.maverickbits.tripguy.screen
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.maverickbits.tripguy.ui.theme.background
+import java.lang.reflect.Modifier
+
+@Preview(showBackground = true, showSystemUi = true, device = "spec:width=411dp,height=891dp")
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun EntryScreen() {
+    Scaffold(topBar = { Toolbar() }, floatingActionButton = {
+        FloatingActionButton(onClick = { }) {
+            Icon(Icons.Default.Add, contentDescription = "Open Bottom Sheet")
+        }
+    })
+    { paddingValues ->
+        Box(
+            androidx.compose.ui.Modifier
+                .fillMaxSize()
+                .background(background) //  Background is properly applied
+                .padding(paddingValues)
+                .padding(0.dp)
+        ) {
+            Row(
+                modifier = androidx.compose.ui.Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Column(
+                    modifier = androidx.compose.ui.Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                        .wrapContentHeight()
+                        .padding(8.dp)
+                        .background(Color.White,shape = RoundedCornerShape(8.dp))
+
+
+                )
+
+                {
+                    Row {
+                        Text(
+                            "Total",
+                            fontSize = 14.sp,
+                            fontWeight = FontWeight.W500,
+                            color = Color.Black,
+                            modifier = androidx.compose.ui.Modifier
+                                .padding(6.dp)
+                        )
+                    }
+                    Column(
+                        modifier = androidx.compose.ui.Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight()
+                            .padding(18.dp),
+                        horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            "Rs 3000/-",
+                            fontSize = 23.sp,
+                            fontWeight = FontWeight.W900,
+                            color = Color.Black
+                        )
+                    }
+
+                }
+                Column(
+                    modifier = androidx.compose.ui.Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                        .weight(1f)
+                        .padding(8.dp)
+                        .background(Color.White,shape = RoundedCornerShape(8.dp))
+                )
+                {
+                    Row {
+                        Text(
+                            "Individual",
+                            fontSize = 14.sp,
+                            fontWeight = FontWeight.W500,
+                            color = Color.Black,
+                            modifier = androidx.compose.ui.Modifier
+                                .padding(6.dp)
+                        )
+                    }
+                    Column(
+                        modifier = androidx.compose.ui.Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight()
+                            .padding(18.dp),
+                        horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            "Rs 1000/-",
+                            fontSize = 23.sp,
+                            fontWeight = FontWeight.W900,
+                            color = Color.Black
+                        )
+                    }
+
+                }
+
+            }
+        }
+    }
+}
+
+
